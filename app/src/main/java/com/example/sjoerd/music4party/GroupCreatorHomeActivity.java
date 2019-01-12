d4dd392206576c2ca6f8a69a2b523b8df76e2de9
sjoerd20 <sjoerd.terpstra@outlook.com>
2019-01-12 11:36:34 +0100

    Implement YoutubePlayer

--- app/src/main/java/com/example/sjoerd/music4party/GroupCreatorHomeActivity.java
+++ app/src/main/java/com/example/sjoerd/music4party/GroupCreatorHomeActivity.java
@@ -4,12 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+
 public class GroupCreatorHomeActivity extends AppCompatActivity {
+
+    private static final String API_KEY = "AIzaSyB91qfzy3kS3ZjtK4YoJ7Wa78afJyY7OHQ ";
+    private static final String VIDEO_CODE = "FoCG-WNsZio";
+    private static final String TAG = GroupCreatorHomeActivity.class.getSimpleName();
+    private YouTubePlayerSupportFragment youTubePlayerSupportFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +41,8 @@ public class GroupCreatorHomeActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        initializeYoutubePlayer();
     }
 
     // inflate options in toolbar
@@ -58,5 +70,31 @@ public class GroupCreatorHomeActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(menuItem);
         }
+    }
+
+    // Initialize youtubeplayer
+    private void initializeYoutubePlayer() {
+        youTubePlayerSupportFragment = (YouTubePlayerSupportFragment) getSupportFragmentManager().
+                findFragmentById(R.id.youtube_player_fragment);
+        if (youTubePlayerSupportFragment == null) {
+            return;
+        }
+        youTubePlayerSupportFragment.initialize(API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+                if (!wasRestored) {
+                    // Set player to default style
+                    youTubePlayer.setPlayerStyle(YouTubePlayer.PlayerStyle.DEFAULT);
+                    // Cue video
+                    youTubePlayer.cueVideo(VIDEO_CODE);
+                }
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.e(TAG, "Youtube Player View initialization failed");
+            }
+        });
+
     }
 }
